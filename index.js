879d18ca879f72b0466f695852025cf38291cf20
Gucci Fan <gucc@umich.edu>
2022-07-27 20:06:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(firstName, userEmail){
+fetch('http://localhost:3000/users', {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify({
+        "name": firstName,
+        "email": userEmail,
+    })
+}
+)
+.then(res => res.json())
+.then(body => addNewData(body.id))
+.catch(function(){
+    document.body.innerHTML = ('Unauthorized Access')
+}
+)
+}
+function addNewData(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID
+}
+submitData()
